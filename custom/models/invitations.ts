bdbe6372ae3d3722f00ec4c2150fb4b9e1d2e097
Val <val.liu@isapsolution.com>
2018-09-06 11:16:14 +0800

    stable

--- custom/models/invitations.ts
+++ custom/models/invitations.ts
@@ -5,14 +5,20 @@ import { Visitors } from './visitors';
 import { Purposes } from './../../custom/models/purposes';
 import { Pin } from 'services/pin-code/pin-code';
 
+export interface IInvitationNotify {
+    visitor: {
+        email: string;
+        phone: string;
+    }
+}
+
 export interface IInvitations {
     parent: Parse.User;
     visitor: Visitors;
     dates: Date[];
     pins: Pin[];
     purpose: Purposes;
-    sendEmail?: boolean;
-    sendSMS?: boolean;
+    notify: IInvitationNotify;
     cancelled?: boolean;
 }
 @registerSubclass() export class Invitations extends ParseObject<IInvitations> {}
