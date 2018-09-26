62a32548a519e66b1b1cdda42fd21705484ef452
Val <val.liu@isapsolution.com>
2018-09-26 14:00:29 +0800

    add test/email test/sms

---
+++ cgi-bin/test/email.ts
@@ -0,0 +1,32 @@
+import {
+    express, Request, Response, Router,
+    Parse, IRole, IUser, RoleList,
+    Action, Errors,
+    Restful, EventSubjects
+} from 'core/cgi-package';
+
+import { ScheduleActionEmail, ScheduleActionEmailResult } from 'models/schedulers/actions';
+
+var action = new Action({
+    loginRequired: true,
+    permission: [RoleList.Administrator, RoleList.SystemAdministrator]
+});
+
+export interface Input {
+    email: string;
+}
+
+action.post<Input>({
+    inputType: "Input"
+}, async (data) => {
+    let result = await new ScheduleActionEmail().do({
+        subject: "test subject",
+        body: "test body",
+        to: [data.inputType.email]
+    });
+
+    return;
+});
+/////////////////////////////////////////////////////
+
+export default action;
---
+++ cgi-bin/test/sms.ts
@@ -0,0 +1,33 @@
+import {
+    express, Request, Response, Router,
+    Parse, IRole, IUser, RoleList,
+    Action, Errors, Config,
+    Restful, EventSubjects
+} from 'core/cgi-package';
+
+import { ScheduleActionSMS, ScheduleActionSMSResult } from 'models/schedulers/actions';
+
+var action = new Action({
+    loginRequired: true,
+    permission: [RoleList.Administrator, RoleList.SystemAdministrator]
+});
+
+export interface Input {
+    phone: string;
+}
+
+action.post<Input>({
+    inputType: "Input"
+}, async (data) => {
+    let result = await new ScheduleActionSMS().do({
+        phone: data.inputType.phone,
+        message: "test message",
+        comPort: Config.sms.comPort,
+        timeout: Config.sms.timeout
+    });
+
+    return;
+});
+/////////////////////////////////////////////////////
+
+export default action;
