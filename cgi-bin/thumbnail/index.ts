969ef8e25f43d4332e171718325cf2b06c7c1e44
Val <val.liu@isapsolution.com>
2018-09-27 17:37:44 +0800

    VMS report API ready

--- cgi-bin/thumbnail/index.ts
+++ cgi-bin/thumbnail/index.ts
@@ -3,18 +3,39 @@ import {
     Parse, IRole, IUser, RoleList, ParseObject,
     Action, Errors,
 } from 'core/cgi-package';
+import * as request from 'request';
+const sharp = require('sharp');
 
 export interface Input {
     url: string;
+    size: number;
 }
 
 var action = new Action<Input>({
-    loginRequired: false,
-    permission: [RoleList.SystemAdministrator, RoleList.Administrator]
+    loginRequired: true
 });
 
 action.get({inputType: "Input"}, async (data) => {
-    
+    let resp = data.response;
+    let { size, url } = data.inputType;
+
+    request({
+        url,
+        method: 'GET',
+        encoding: null
+    }, async (err, res, body) => {
+        if (err) {
+            resp.sendStatus(404);
+            return;
+        }
+
+        body = await sharp(body)
+            .resize(size)
+            .toBuffer();
+
+        resp.setHeader("content-type", res.headers["content-type"]);
+        resp.end(body, "binary");
+    });
 });
 
 export default action;
