bd1ec89acacde6dffee8880bd7670417765e85f3
Val <val.liu@isapsolution.com>
2018-10-22 11:18:35 +0800

    stable

--- custom/models/index.ts
+++ custom/models/index.ts
@@ -1,3 +1,7 @@
+/**
+ * ALARM: DON'T MODIFY THIS FILE.
+ * this file is auto created. whatever you insert into this file will be overwritten.
+ */
 export * from './companies';
 export * from './floors';
 export * from './invitations';
