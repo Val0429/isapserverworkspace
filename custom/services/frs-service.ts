0ab7bbc91f399ea4dc56b8bbc3410e4969aa5853
Val <val.liu@isapsolution.com>
2018-11-23 17:58:09 +0800

    stable

--- custom/services/frs-service.ts
+++ custom/services/frs-service.ts
@@ -1,70 +1,49 @@
-import config from './../../config/custom/frs';
+import { Config } from 'core/config.gen';
 import * as request from 'request';
-import * as http from 'http';
-import { Log } from 'helpers/utility';
+import { Observable, BehaviorSubject, Subject, Observer } from 'rxjs';
+import { Response } from '~express/lib/response';
+
 
 export class FRSService {
-    session_id: string;
+    private sessionId: string;
+    private sjLogined: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
     constructor() {
         this.login();
     }
 
-    private maintainInterval;
-    login() {
-        const url: string = `http://${config.ip}:${config.port}/frs/cgi/login`;
-
-        var tryLogin = () => {
+    /// public functions ////////////////////
+    snapshot(image: string, resp: Response = null): Promise<string> {
+        return new Promise<string>( async (resolve, reject) => {
+            await this.waitForLogin();
+            var url: string = this.makeUrl(`snapshot/session_id=${this.sessionId}&image=${image}`);
             request({
                 url,
-                method: 'POST',
-                json: true,
-                body: { username: config.account, password: config.password }
+                method: 'GET',
+                encoding: null
             }, (err, res, body) => {
-                if (err || !body.session_id) {
-                    Log.Error('FRS Server', `Login failed@${config.ip}:${config.port}. Retry in 1 second.`);
-                    setTimeout(() => { tryLogin() }, 1000);
-                    return;
+                if (err) { reject(err); return; }
+                if (resp !== null) {
+                    resp.setHeader("content-type", res.headers["content-type"]);
+                    resp.end(body, "binary");
                 }
-                Log.Info('FRS Server', `Login into Server@${config.ip}:${config.port}.`);
-                this.session_id = body.session_id;
-                /// After login and got session_id, maintain session every 1 minute.
-                this.maintainInterval && clearInterval(this.maintainInterval);
-                this.maintainInterval = setInterval( () => {
-                    this.maintainSession();
-                }, 60000);
+                resolve(body);
             });
-        }
-        tryLogin();
-    }
-    
-    maintainSession() {
-        const url: string = `http://${config.ip}:${config.port}/frs/cgi/maintainsession`;
-        request({
-            url,
-            method: 'POST',
-            json: true,
-            body: { session_id: this.session_id }
-        }, (err, res, body) => {
-            if (err) {
-                Log.Error('FRS Server', `Maintain FRS session failed@${config.ip}:${config.port}.`);
-                this.maintainInterval && clearInterval(this.maintainInterval);
-                this.login();
-            }
-            // console.log('maintain success', body);
         });
     }
 
     compareFace(image_1: string, image_2: string): Promise<number> {
-        const url: string = `http://${config.ip}:${config.port}/frs/cgi/compareface`;
-        return new Promise((resolve, reject) => {
+        const url: string = this.makeUrl('compareface');
+        return new Promise( async (resolve, reject) => {
+            await this.waitForLogin();
             request({
                 url,
                 method: 'POST',
                 json: true,
-                body: { session_id: this.session_id, image_1, image_2 }
+                body: { session_id: this.sessionId, image_1, image_2 }
             }, (err, res, body) => {
                 if (err) {
-                    Log.Error('FRS Server', `Compare face failed. ${err}`);
+                    console.log(`Compare face failed. ${err}`);
                     reject(err); return;
                 }
                 if (body.score === undefined) { reject(body); return; }
@@ -73,6 +52,104 @@ export class FRSService {
         });
     }
 
+    getGroupList(): Promise<GroupInfo[]> {
+        const url = this.makeUrl("getgrouplist");
+        return new Promise( async (resolve, reject) => {
+            await this.waitForLogin();
+            request({
+                url,
+                method: 'POST',
+                json: true,
+                body: { session_id: this.sessionId, page_size: 999, skip_pages: 0 }
+            }, (err, res, body) => {
+                if (err) {
+                    console.log(`Get group list failed. ${err}`);
+                    reject(err); return;
+                }
+                resolve(body.group_list.groups);
+            });
+        });
+    }
+    /////////////////////////////////////////
+
+    /// private functions ///////////////////
+    private loggingIn: boolean = false;
+    private maintainTimer: NodeJS.Timer = null;
+    private login() {
+        const url = this.makeUrl("login");
+
+        let tryLogin = () => {
+            if (this.loggingIn === true) return;
+            this.loggingIn = true;
+
+            request({
+                url, method: 'POST', json: true,
+                body: { username: Config.frs.account, password: Config.frs.password }
+            }, (err, res, body) => {
+                this.loggingIn = false;
+                if (err || !body) {
+                    console.log(`Login FRS Server failed@${Config.frs.ip}:${Config.frs.port}. Retry in 1 second.`);
+                    setTimeout(() => { tryLogin() }, 1000);
+                    return;
+                }
+
+                this.sjLogined.next(true);
+                console.log(`Login into FRS Server@${Config.frs.ip}:${Config.frs.port}.`);
+
+                this.sessionId = body.session_id;
+                /// After login and got session_id, maintain session every 1 minute.
+                if (this.maintainTimer !== null) clearInterval(this.maintainTimer);
+                this.maintainTimer = setInterval( async () => {
+                    var result = await this.maintainSession();
+                    if (!result) clearInterval(this.maintainTimer);
+                }, 60000);
+                //this.doWebsocketListen();
+            });
+            
+        }
+        tryLogin();
+    }
+
+    private maintainSession(): Promise<boolean> {
+        const url: string = this.makeUrl('maintainsession');
+        var me = this;
+
+        return new Promise( (resolve, reject) => {
+            request({
+                url, method: 'POST', json: true,
+                body: { session_id: this.sessionId }
+            }, (err, res, body) => {
+                if (!body || body.message === 'Unauthorized.') {
+                    this.sjLogined.next(false);
+                    console.log(`Maintain FRS session failed@${Config.frs.ip}:${Config.frs.port}.`);
+                    resolve(false);
+                    me.login();
+                    return;
+                }
+                console.log('maintain success', body);
+                resolve(true);
+            });
+
+        });
+    }
+    /////////////////////////////////
+
+    /// private helpers /////////////
+    private makeUrl(func: string) {
+        const urlbase: string = `http://${Config.frs.ip}:${Config.frs.port}/frs/cgi`;
+        return `${urlbase}/${func}`;
+    }
+    private waitForLogin() {
+        return this.sjLogined.getValue() === true ? null :
+            this.sjLogined.filter(val => val === true).first().toPromise();
+    }
+    /////////////////////////////////
 }
 
 export default new FRSService();
+
+
+interface GroupInfo {
+    name: string;
+    group_id: string;
+}
--- main.ts
+++ main.ts
@@ -4,3 +4,10 @@ import './custom/services/frs-service';
 import './custom/schedulers/index';
 import './custom/shells/index';
 import 'services/pin-code/pin-code';
+
+
+import frs from './custom/services/frs-service';
+(async () => {
+    let result = await frs.getGroupList();
+    console.log('???', result);
+})();
