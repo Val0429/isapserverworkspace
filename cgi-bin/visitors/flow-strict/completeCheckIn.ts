598b98eec29d9c99ba2b03273d848387a902a2cd
Val <val.liu@isapsolution.com>
2018-12-06 17:21:07 +0800

    todo fix

--- cgi-bin/visitors/flow-strict/completeCheckIn.ts
+++ cgi-bin/visitors/flow-strict/completeCheckIn.ts
@@ -1,9 +1,11 @@
 import {
     express, Request, Response, Router,
     Parse, IRole, IUser, RoleList,
-    Action, Errors, Person, ParseObject,
-    Events, EventStrictCompleteCheckIn
+    Action, Errors, Person, ParseObject, FileHelper,
+    Events, EventStrictCompleteCheckIn, EventStrictCompareFace
 } from 'core/cgi-package';
+
+import frs from './../../../custom/services/frs-service';
 
 import { Pin } from 'services/pin-code/pin-code';
 import { Invitations, IInvitationDateAndPin } from './../../../custom/models/invitations';
@@ -26,6 +28,7 @@ export default new Action<Input, Output>({
     let { owner, invitation, result, company, visitor, index } = await tryCheckInWithPinCode(pin);
     let kiosk = data.user;
     let eventData = { owner, pin, invitation, company, visitor, kiosk };
+    let visitorName = visitor.getValue("name");
 
     let saveEvent = () => {
         /// save event
@@ -43,6 +46,42 @@ export default new Action<Input, Output>({
     });
 
     saveEvent();
+
+    
+    /// enroll into FRS
+    /// 1) get all groups
+    /// 1.1) find visitor group. if no go 1.2)
+    /// 1.2) create visitor group
+    /// 2) create person
+    /// 2.1) find last compare face
+    /// 2.2) create person
+    /// 2.3) add person into group
+    
+    /// 1)
+    let groups = await frs.getGroupList();
+    /// 1.1)
+    let groupid: string = groups.reduce<string>( (final, value) => {
+        if (final) return final;
+        if (value.name === 'Visitor') return value.group_id;
+        return final;
+    }, undefined);
+    /// 1.2)
+    if (groupid === undefined) {
+        let res = await frs.createGroup("Visitor");
+        groupid = res.group_id;
+    }
+    /// 2)
+    /// 2.1)
+    let lastEvent: EventStrictCompareFace = await new Parse.Query(EventStrictCompareFace)
+        .equalTo("visitor", visitor)
+        .descending("createdAt")
+        .first();
+    if (!lastEvent) throw Errors.throw(Errors.CustomBadRequest, ["Should compare face first before check in."]);
+    /// 2.2)
+    let image = (await FileHelper.downloadParseFile( lastEvent.getValue("image") )).toString("base64");
+    let person = await frs.createPerson(visitorName, image);
+    /// 2.3)
+    await frs.applyGroupsToPerson(person.person_id, groupid);
 
     return ParseObject.toOutputJSON(invitation);
 });
