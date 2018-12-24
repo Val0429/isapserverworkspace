d0b93f732cc4509114c2c2a740a894dcb448e2f2
Val <val.liu@isapsolution.com>
2018-12-24 16:27:36 +0800

    simulate first API - FRS search record & complete

--- custom/agents/frs-agent/index.ts
+++ custom/agents/frs-agent/index.ts
@@ -3,8 +3,17 @@ import { Agent } from 'models/agents';
 import { IFRSServiceConfig, RecognizedUser, UnRecognizedUser } from 'workspace/custom/services/frs-service/libs/core';
 import { Subject, Observable } from 'rxjs';
 
+import { FRSService } from 'workspace/custom/services/frs-service';
+import 'workspace/custom/services/frs-service/modules/live-faces';
+import 'workspace/custom/services/frs-service/modules/search-records';
+
 interface IEnableLiveFaces {
     enable: boolean;
+}
+
+interface ISearchRecords {
+    starttime: Date;
+    endtime: Date;
 }
 
 @Agent.Register({
@@ -15,23 +24,33 @@ interface IEnableLiveFaces {
     }
 })
 export class FRSAgent extends Agent.Base<IFRSServiceConfig> {
+    private frs: FRSService;
+    constructor(config: IFRSServiceConfig, remote?: Agent.IRemoteAgent) {
+        super(config, remote);
+        this.frs = new FRSService(config);
+    }
+
     public Start() {
-        
+        this.frs.start();
     }
 
     public Stop() {
-        
+        this.frs.stop();
     }
 
-    private sjLiveFaces: Subject<RecognizedUser | UnRecognizedUser> = new Subject();
     @Agent.Function({
         inputType: "IEnableLiveFaces",
         description: "Enable live recognized face & unrecognized face via websocket."
     })
     public EnableLiveFaces(config: IEnableLiveFaces): Observable<RecognizedUser | UnRecognizedUser> {
-        return this.sjLiveFaces.asObservable();
+        this.frs.enableLiveFaces(config.enable);
+        return this.frs.sjLiveStream.asObservable();
+    }
+
+    @Agent.Function({
+        
+    })
+    public SearchRecords(config: ISearchRecords): Observable<RecognizedUser | UnRecognizedUser> {
+        return this.frs.searchRecords(config.starttime, config.endtime, undefined, 10).asObservable();
     }
 }
-
-// console.log('???')
-// new FRSAgent({} as any);
--- main.ts
+++ main.ts
@@ -9,3 +9,22 @@ import { Agent } from 'models/agents';
 
 
 console.log('agents?', JSON.stringify(Agent.all(), null, 2) );
+
+import { FRSAgent } from './custom/agents/frs-agent';
+let frs = new FRSAgent({
+    frs: {
+        ip: "172.16.10.88",
+        port: 8088,
+        wsport: 7077,
+        account: "val",
+        password: "123456"
+    }
+});
+frs.Start();
+frs.SearchRecords({
+    starttime: new Date(2018, 11, 10, 0,0,0),
+    endtime: new Date(2018, 11, 10, 23,59,59)
+}).subscribe( (data) => {
+    console.log({...data, face_feature: undefined});
+}, () => {}, () => console.log('search complete'));
+    
