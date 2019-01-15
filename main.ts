f9a4bb42f180923458d149b726658ebf2eccd575
Min.Hsieh <a1991704@yahoo.com.tw>
2019-01-15 15:41:47 +0800

    Add static file

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
+import * as express from 'express';
 import { app } from './../core/main.gen';
 
 import './custom/schedulers/index';
 import './custom/shells/index';
 import 'services/pin-code/pin-code';
 import './custom/services/hd';
+// import './custom/services/weigand';
+
+app.use('/snapshots', express.static('workspace/custom/assets/snapshots'));
