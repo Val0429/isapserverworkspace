ea0dc0364cd6cb802edbe57e39ba1338051ba615
Min.Hsieh <a1991704@yahoo.com.tw>
2019-01-23 10:07:04 +0800

    Clean project

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ import { app } from './../core/main.gen';
 
 import './custom/schedulers/index';
 import './custom/shells/index';
-import 'services/pin-code/pin-code';
 // import './custom/services/hd';
 // import './custom/services/weigand';
 
