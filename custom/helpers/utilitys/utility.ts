9cdcc8af57a8c33e2f3ee7b30298c3f76a7f49fe
Min.Hsieh <a1991704@yahoo.com.tw>
2019-01-24 12:03:15 +0800

    Add show running address at startup

--- custom/helpers/utilitys/utility.ts
+++ custom/helpers/utilitys/utility.ts
@@ -1,3 +1,5 @@
+import * as Os from 'os';
+
 export namespace Utility {
     /**
      * String pad left some char
@@ -26,5 +28,41 @@ export namespace Utility {
         regex = regex.replace(/\|$/, '');
 
         return new RegExp(regex, 'g');
+    }
+
+    /**
+     * Interface with ip
+     */
+    export interface IIp {
+        name: string;
+        family: string;
+        ip: string;
+    }
+
+    /**
+     * Get ip list
+     */
+    export function GetIp(): IIp[] {
+        let ifaces = Os.networkInterfaces();
+
+        let ips: IIp[] = new Array<IIp>()
+            .concat(
+                ...Object.keys(ifaces).map((ifname) => {
+                    return ifaces[ifname].map((iface) => {
+                        if ('IPv4' === iface.family && iface.internal === false) {
+                            return {
+                                name: ifname,
+                                family: iface.family,
+                                ip: iface.address,
+                            };
+                        }
+                    });
+                }),
+            )
+            .filter((value, index, array) => {
+                return value !== undefined;
+            });
+
+        return ips;
     }
 }
--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
 import * as express from 'express';
-import { app } from './../core/main.gen';
-
+import { app } from 'core/main.gen';
+import { Config } from 'core/config.gen';
+import { Print, Utility } from './custom/helpers';
 import './custom/schedulers/index';
 import './custom/shells/index';
+// import './custom/services/demo';
 // import './custom/services/hd';
 // import './custom/services/weigand';
 
 app.use('/snapshots', express.static('workspace/custom/assets/snapshots'));
+
+setTimeout(() => {
+    Print.Message({ message: '  ', background: Print.BackColor.green }, { message: 'App running at:', color: Print.FontColor.green }, { message: '- Local:  ' }, { message: `http://localhost:${Config.core.port}`, color: Print.FontColor.cyan });
+
+    let ips: Utility.IIp[] = Utility.GetIp();
+    if (ips.length > 0) {
+        Print.Message({ message: '  ', background: Print.BackColor.green }, { message: 'App running at:', color: Print.FontColor.green }, { message: '- Network:' }, { message: `http://${ips[0].ip}:${Config.core.port}`, color: Print.FontColor.cyan });
+    }
+}, 30);
