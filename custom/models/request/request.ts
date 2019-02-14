36674302a4cd509f8ae12a3631be231b2d91e781
Min.Hsieh <a1991704@yahoo.com.tw>
2019-02-14 09:03:29 +0800

    Add Config cgi

--- custom/models/request/request.ts
+++ custom/models/request/request.ts
@@ -1,8 +1,9 @@
+import * as IConfig from './config';
 import * as IFaceCount from './faceCount';
 import * as IOccupancy from './occupancy';
 import * as IPrinter from './printer';
 
-export { IFaceCount, IOccupancy, IPrinter };
+export { IConfig, IFaceCount, IOccupancy };
 
 export interface IDataList {
     page?: number;
