36c221dda8135a4c49f17fa2acb76bef976d79e9
Val <val.liu@isapsolution.com>
2019-02-18 14:20:58 +0800

    stable

--- main.ts
+++ main.ts
@@ -34,57 +34,145 @@ interface IRegion {
 }
 @registerSubclass({memoryCache: true, container: true}) export class Region extends Tree<IRegion> {}
 
-@registerSubclass({memoryCache: true}) export class RegionPersonSchedule extends Schedule.Of(Person, Region) {}
+@registerSubclass({memoryCache: true}) export class RegionPersonSchedule extends Schedule.Of({who: Person, where: Region}) {}
+@registerSubclass({memoryCache: true}) export class RegionBasicSchedule extends Schedule.Of({where: Region}) {}
 
 /// make permission
-interface IRegionPermission {
-    all?: boolean;
-    read?: boolean;
-    write?: boolean;
-    delete?: boolean;
+enum EAccessRight {
+    Access = 0x1,
+    Alert = 0x2,
+    AutoAirCondition = 0x4
 }
-@registerSubclass({memoryCache: true}) export class RegionPermission extends Permission.Of(Region).With<IRegionPermission>().On(Person, Group) {}
+interface IRegionPermission {
+    access?: EAccessRight;
+}
+// @registerSubclass({memoryCache: true}) export class RegionPermission extends Permission.Of(Region).With<IRegionPermission>().On(Person, Group) {}
 
-class test1 extends ParseObject<any> {}
-class test2 extends test1 {}
-console.log('instanceof?', test1 instanceof ParseObject);
-console.log('instanceof?', test2 instanceof ParseObject);
+@registerSubclass({memoryCache: true}) export class RegionPermission extends Permission.Of(Region).With<IRegionPermission>().On(RegionPersonSchedule, Person, Group, RegionBasicSchedule) {}
 
-//@registerSubclass({memoryCache: true}) export class RegionPermission extends Permission.Of(Region).With<IRegionPermission>().On(RegionPersonSchedule, Person, Group) {}
+
 
 (async () => {
 
     await serverReady;
 
     let person = await new Parse.Query(Person).equalTo("name", "Min").first();
+    let personFrank = await new Parse.Query(Person).equalTo("name", "Frank").first();
     let region = await new Parse.Query(Region).equalTo("name", "Taipei").first();
     let group = await new Parse.Query(Group).equalTo("name", "Super VIP").first();
 
-    // console.log('group', group)
-    // setTimeout( async () => {
-    //     console.log('main...')
-    //     Log.TraceTime("main", "time for create person");
-    //     let person = await new Parse.Query(Person).equalTo("name", "Min").first();
-    //     Log.TraceTimeEnd("main", "time for create person");
-    // }, 3000)
+    // let rpsDay = new RegionBasicSchedule({
+    //     where: region,
+    //     when: {
+    //         beginDate: new Date(2019, 0, 1, 8, 0, 0),
+    //         endDate: new Date(2019, 0, 1, 18, 0, 0),
+    //         fullDay: false,
+    //         repeat: {
+    //             type: EScheduleUnitRepeatType.Week,
+    //             value: 1,
+    //             data: [1,2,3,4,5],
+    //             endType: EScheduleUnitRepeatEndType.NoStop
+    //         }
+    //     }
+    // });
+    // await rpsDay.save();
+    // let rpsNight = new RegionBasicSchedule({
+    //     where: region,
+    //     when: {
+    //         beginDate: new Date(2019, 0, 1, 18, 0, 0),
+    //         endDate: new Date(2019, 0, 2, 8, 0, 0),
+    //         fullDay: false,
+    //         repeat: {
+    //             type: EScheduleUnitRepeatType.Week,
+    //             value: 1,
+    //             data: [1,2,3,4,5],
+    //             endType: EScheduleUnitRepeatEndType.NoStop
+    //         }
+    //     }
+    // });
+    // await rpsNight.save();
+    // let rpsOff = new RegionBasicSchedule({
+    //     where: region,
+    //     when: {
+    //         beginDate: new Date(2019,0,5,0,0,0),
+    //         endDate: new Date(2019,0,7,0,0,0),
+    //         fullDay: false,
+    //         repeat: {
+    //             type: EScheduleUnitRepeatType.Week,
+    //             value: 1,
+    //             data: [6],
+    //             endType: EScheduleUnitRepeatEndType.NoStop
+    //         }
+    //     },
+    //     priority: 60
+    // });
+    // await rpsOff.save();
+    // await RegionPermission.set(region, rpsDay, "access", EAccessRight.Access);
+    // await RegionPermission.set(region, rpsNight, "access", EAccessRight.Access | EAccessRight.Alert);
+    // await RegionPermission.set(region, rpsOff, "access", EAccessRight.Alert);
 
-    // // RegionPermission.set(region, group, {
-    // //     read: true, write: false
-    // // });
+    // let rpsFrankDay = new RegionPersonSchedule({
+    //     who: personFrank,
+    //     where: region,
+    //     when: {
+    //         beginDate: new Date(2019, 0, 1, 8, 0, 0),
+    //         endDate: new Date(2019, 0, 1, 18, 0, 0),
+    //         fullDay: false,
+    //         repeat: {
+    //             type: EScheduleUnitRepeatType.Week,
+    //             value: 1,
+    //             data: [1,2,3,4,5],
+    //             endType: EScheduleUnitRepeatEndType.NoStop
+    //         }
+    //     }
+    // });
+    // await rpsFrankDay.save();
+    // let rpsFrankNight = new RegionPersonSchedule({
+    //     who: personFrank,
+    //     where: region,
+    //     when: {
+    //         beginDate: new Date(2019, 0, 1, 18, 0, 0),
+    //         endDate: new Date(2019, 0, 2, 8, 0, 0),
+    //         fullDay: false,
+    //         repeat: {
+    //             type: EScheduleUnitRepeatType.Week,
+    //             value: 1,
+    //             data: [1,2,3,4,5],
+    //             endType: EScheduleUnitRepeatEndType.NoStop
+    //         }
+    //     }
+    // });
+    // await rpsFrankNight.save();
+    // await RegionPermission.set(region, rpsFrankDay, "access", EAccessRight.Access | EAccessRight.AutoAirCondition);
+    // await RegionPermission.set(region, rpsFrankNight, "access", EAccessRight.Access);
 
-    // // RegionPermission.set(region, person, {
-    // //     read: true,
-    // //     write: false
-    // // });
+
+    Log.TraceTime('Main', 'time for permission verify')
+    let promises = [];
+    for (let i=0; i<1; ++i) {
+        promises.push( RegionPermission.verify(region, person, "access", { date: new Date(2019,1,15,7,0,0) }) );
+    }
+    await Promise.all(promises);
+    Log.TraceTimeEnd('Main', 'time for permission verify')
+
 
     // Log.TraceTime('Main', 'time for permission verify')
-    // let result = await RegionPermission.verify(region, group, "all");
+    // let result = await RegionPermission.verify(region, personFrank, "access", { date: new Date(2019,1,15,7,0,0) });
     // Log.TraceTimeEnd('Main', 'time for permission verify')
     // console.log('final result!', result);
+
+    // Log.TraceTime('Main', 'time for permission verify')
+    // let result2 = await RegionPermission.verify(region, person, "access", { date: new Date(2019,1,15,7,0,0) });
+    // Log.TraceTimeEnd('Main', 'time for permission verify')
+    // console.log('final result!', result2);
 
     // // console.time('time for permission verify')
     // // result = await RegionPermission.verify(region, person, "read");
     // // console.timeEnd('time for permission verify')
+
+    // let cal = await RegionPersonSchedule.buildCalendar(person, { start: new Date(2019,1,16,7,0,0), end: new Date(2019,1,16,7,0,0) });
+    // console.log('Cal!', cal)
+    // console.log('Cal!', cal.matchTime(new Date(2019,1,16,7,0,0)));
 
 })();
 
@@ -154,69 +242,10 @@ console.log('instanceof?', test2 instanceof ParseObject);
         //     }
         // });
         // await rps.save();
-        let cal = await RegionPersonSchedule.buildCalendar(PersonMin, { start: new Date(2018, 11, 29, 0,0,0,0), end: new Date(2030,1,5,0,0,0,0) });
-        console.log('Cal!', cal.matchTime(new Date(2019,4,12,16,30,0)));
+        // let cal = await RegionPersonSchedule.buildCalendar(PersonMin, { start: new Date(2018, 11, 29, 0,0,0,0), end: new Date(2030,1,5,0,0,0,0) });
+        // console.log('Cal!', cal.matchTime(new Date(2019,4,12,16,30,0)));
 
     } catch(e) { console.log('catched', JSON.stringify(e)) }
 
 })();
 
-
-
-
-// enum ECalendarUnitRepeatType {
-//     NoRepeat = 0x0000,
-//     Day = 0x1000,
-//     WeekDay = 0x1500,
-//     Week = 0x2000,
-//     Month = 0x3000,
-//     Year = 0x10000,
-// }
-
-// enum ECalendarUnitRepeatIntervalType {
-//     NoRepeat = 0x0000,
-//     Day = 0x1000,
-//     Week = 0x2000,
-//     Month = 0x3000,
-//     Year = 0x10000,
-// }
-
-// enum ECalendarUnitRepeatEndType {
-//     NoStop = 1,
-//     Date = 2,
-//     TotalTimes = 3
-// }
-
-// interface ICalendarUnitRepeatInterval {
-//     /// 重複類別: 天週月年
-//     type: ECalendarUnitRepeatIntervalType;
-//     /// 重複間隔
-//     value: number;
-//     /// 重複資料
-//     /// Day: never;
-//     /// Week: number[];
-//     /// Month: By this Day | By this Weekday
-//     /// Year: never;
-//     data: any;
-// }
-
-// interface ICalendarUnitRepeat {
-//     type: ECalendarUnitRepeatType;
-
-//     repeatInterval: ICalendarUnitRepeatInterval;
-
-//     /// 結束時間類別
-//     repeatEndType: ECalendarUnitRepeatEndType;
-//     /// 結束時間 Date of Stop, or Number or Times
-//     repeatEndValue: any;
-// }
-
-// interface ICalendarUnit {
-//     beginDate: Date;
-//     endDate: Date;
-//     fullDay: boolean;
-
-//     repeat: ICalendarUnitRepeat;
-//     /// 計算出來的結束時間, by repeatEndDate & endDate
-//     calculatedEndDate?: Date;
-// }
