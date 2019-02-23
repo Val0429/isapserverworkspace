ba461395d1ffbf8f08a10b7e1897c454b5d49aa6
Min.Hsieh <a1991704@yahoo.com.tw>
2019-02-23 20:26:22 +0800

    Add Template cgi

---
+++ cgi-bin/template/index.ts
@@ -0,0 +1,73 @@
+import { IUser, Action, Restful, RoleList, Errors } from 'core/cgi-package';
+import { IRequest, IResponse } from '../../custom/models';
+
+let action = new Action({
+    loginRequired: true,
+    permission: [RoleList.SystemAdministrator, RoleList.Administrator, RoleList.Chairman, RoleList.DeputyChairman, RoleList.FinanceCommittee, RoleList.DirectorGeneral, RoleList.Guard],
+});
+
+export default action;
+
+/**
+ * Action Create
+ */
+type InputC = string;
+
+type OutputC = string;
+
+action.post(
+    { inputType: 'InputC' },
+    async (data): Promise<OutputC> => {
+        let _input: InputC = data.inputType;
+
+        return '';
+    },
+);
+
+/**
+ * Action Read
+ */
+type InputR = string;
+
+type OutputR = string;
+
+action.get(
+    { inputType: 'InputR' },
+    async (data): Promise<OutputR> => {
+        let _input: InputR = data.inputType;
+
+        return '';
+    },
+);
+
+/**
+ * Action update
+ */
+type InputU = string;
+
+type OutputU = string;
+
+action.put(
+    { inputType: 'InputU' },
+    async (data): Promise<OutputU> => {
+        let _input: InputU = data.inputType;
+
+        return '';
+    },
+);
+
+/**
+ * Action Delete
+ */
+type InputD = string;
+
+type OutputD = string;
+
+action.delete(
+    { inputType: 'InputD' },
+    async (data): Promise<OutputD> => {
+        let _input: InputD = data.inputType;
+
+        return '';
+    },
+);
