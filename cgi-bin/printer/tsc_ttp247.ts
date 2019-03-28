3be8e7106d834c2582d114a7a097264576f8c299
Min.Hsieh <a1991704@yahoo.com.tw>
2019-03-28 13:27:02 +0800

    Upgrade Printer post cgi

--- cgi-bin/printer/tsc_ttp247.ts
+++ cgi-bin/printer/tsc_ttp247.ts
@@ -1,6 +1,6 @@
 import { IUser, Action, Restful, RoleList, Errors } from 'core/cgi-package';
 import { Config } from 'core/config.gen';
-import { Tsc_Ttp247, File, Print } from 'workspace/custom/helpers';
+import { Tsc_Ttp247, Print, DateTime } from 'workspace/custom/helpers';
 import { IRequest } from '../../custom/models';
 
 let action = new Action({
@@ -11,7 +11,33 @@ let action = new Action({
 export default action;
 
 /**
- * Action Read
+ * Action
+ */
+type InputC = IRequest.IPrinter.ITsc_ttp247R;
+
+type OutputC = string;
+
+action.post(
+    { inputType: 'InputC' },
+    async (data): Promise<OutputC> => {
+        let _input: InputC = data.inputType;
+        let _date: string = _input.date || '';
+        let _location: string = _input.locationName || '';
+
+        let tsc: Tsc_Ttp247 = new Tsc_Ttp247();
+        tsc.device = Config.printer.device;
+
+        tsc.Initialization();
+
+        let result: string = await tsc.PrintFetSticker(_input.visitorName, _input.respondentName, _location, _date);
+        Print.MinLog(`${DateTime.DateTime2String(new Date())}: ${JSON.stringify(_input)}`);
+
+        return JSON.stringify(_input);
+    },
+);
+
+/**
+ * Action
  */
 type InputR = IRequest.IPrinter.ITsc_ttp247R;
 
@@ -30,7 +56,8 @@ action.get(
         tsc.Initialization();
 
         let result: string = await tsc.PrintFetSticker(_input.visitorName, _input.respondentName, _location, _date);
+        Print.MinLog(`${DateTime.DateTime2String(new Date())}: ${JSON.stringify(_input)}`);
 
-        return result;
+        return JSON.stringify(_input);
     },
 );
