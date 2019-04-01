72d3b0d245243667ab626f1df513f0222225e9ac
Min.Hsieh <a1991704@yahoo.com.tw>
2019-04-01 14:32:14 +0800

    Clean project

--- custom/helpers/printer/base.ts
+++
@@ -1,8 +0,0 @@
-export namespace Printer {
-    /**
-     *
-     */
-    export enum Message {
-        DeviceNotInitialization = 'Device is not initialization yet',
-    }
-}
--- custom/schedulers/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * ALARM: DON'T MODIFY THIS FILE.
- * this file is auto created. whatever you insert into this file will be overwritten.
- */
-export default undefined;
