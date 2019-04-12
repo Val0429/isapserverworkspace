6ed20a1f0aff2d2c18c483fa7bb872c25ff47560
Min.Hsieh <a1991704@yahoo.com.tw>
2019-04-12 17:19:25 +0800

    Change server port

--- config/default/core.ts
+++ config/default/core.ts
@@ -1,13 +1,13 @@
 import { Config } from 'config_default/core';
 
 var config: Partial<Config> = {
-    port: 6060,
+    port: 6061,
     disableCache: true,
     accessControlAllowOrigin: true,
     cgiPath: '',
     httpDisabled: false,
     httpsEnabled: true,
-    httpsPort: 4443,
+    httpsPort: 4444,
 };
 export default config;
 export { Config };
