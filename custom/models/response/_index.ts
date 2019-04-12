7968f0edc467e19d076516a161c1a87e4d16e225
Min.Hsieh <a1991704@yahoo.com.tw>
2019-04-12 17:43:18 +0800

    Add License cgi

--- custom/models/response/_index.ts
+++ custom/models/response/_index.ts
@@ -1,6 +1,7 @@
 import * as ICommunity from './community';
 import * as IConfig from './config';
 import * as IGas from './gas';
+import * as ILicense from './license';
 import * as IListen from './listen';
 import * as IManageCost from './managecost';
 import * as IParking from './parking';
@@ -13,7 +14,7 @@ import * as IUser from './user';
 import * as IVisitor from './visitor';
 import * as IVote from './vote';
 
-export { ICommunity, IConfig, IGas, IListen, IManageCost, IParking, IPublicArticle, IPublicCalendar, IPublicFacility, IPublicNotify, IPackage, IUser, IVisitor, IVote };
+export { ICommunity, IConfig, IGas, ILicense, IListen, IManageCost, IParking, IPublicArticle, IPublicCalendar, IPublicFacility, IPublicNotify, IPackage, IUser, IVisitor, IVote };
 
 export interface IDataList<T> {
     total: number;
---
+++ custom/models/response/license.ts
@@ -0,0 +1,12 @@
+export interface IIndexR {
+    licenseKey: string;
+    description: string;
+    mac: string;
+    brand: string;
+    productNO: string;
+    count: number;
+    trial: boolean;
+    registerDate: string;
+    expireDate: string;
+    expired: boolean;
+}
