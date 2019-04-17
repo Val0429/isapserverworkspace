bb4f363f070b5c4ce196b725b5737cf625b00836
Min.Hsieh <a1991704@yahoo.com.tw>
2019-04-17 14:30:47 +0800

    v1.00.02

--- nsis/install_mongo.bat
+++ nsis/install_mongo.bat
@@ -1,14 +1,14 @@
 c:
 cd \
 cd "C:\Program Files\MongoDB"
-md data_printer
-cd data_printer
+md data_isap
+cd data_isap
 md db
 cd ..
-md log_printer
+md log_isap
 
 cd "C:\Program Files\MongoDB\Server\3.6\bin"
-mongod --port 27017 --dbpath "C:\Program Files\MongoDB\data_printer" --logpath="C:\Program Files\MongoDB\log_printer\log.txt" --install --serviceName "iSap Printer MongoDB" --serviceDisplayName "iSap Printer MongoDB" --replSet rs0
-net start "iSap Printer MongoDB"
+mongod --port 27017 --dbpath "C:\Program Files\MongoDB\data_isap" --logpath="C:\Program Files\MongoDB\log_isap\log.txt" --install --serviceName "iSap MongoDB" --serviceDisplayName "iSap MongoDB" --replSet rs0
+net start "iSap MongoDB"
 mongo --port 27017 --eval "rs.initiate({_id : 'rs0', members : [{_id : 0, host : 'localhost:27017'}]})"
 mongo --port 27017 --eval "while(true) {if (rs.status().ok) break;sleep(1000)};"
