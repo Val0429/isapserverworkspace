f04507f861ff27e993584ff694694a0691cdbe17
Min.Hsieh <a1991704@yahoo.com.tw>
2019-04-18 13:47:04 +0800

    Upgrade License Product Id

---
+++ config/custom/people-counting.ts
@@ -0,0 +1,10 @@
+export interface Config {
+    productId: string;
+    intervalSecond: number;
+}
+
+let config: Config = {
+    productId: '00106',
+    intervalSecond: 300,
+};
+export default config;
