369eee851a4a52fba5d718f57b2c28d2d83e5609
Min.Hsieh <a1991704@yahoo.com.tw>
2019-04-25 11:02:06 +0800

    Upgrade Create Index shell

--- custom/shells/create-index.ts
+++ custom/shells/create-index.ts
@@ -6,4 +6,11 @@ import { createIndex } from 'helpers/parse-server/parse-helper';
     if (!Config.mongodb.enable) {
         return;
     }
+
+    createIndex('Camera', 'cameraIndex', { type: 1 });
+    createIndex('LocationArea', 'locationAreaIndex', { floor: 1 });
+    createIndex('LocationDevice', 'locationDeviceIndex', { floor: 1, area: 1, type: 1 });
+    createIndex('ReportHumanDetectionSummary', 'reportHDSummaryIndex', { type: 1, date: 1, floor: 1, area: 1 });
+    createIndex('ReportHumanDetection', 'reportHDIndex', { date: 1, floor: 1, area: 1, total: 1 });
+    createIndex('UserInfo', 'userInfoIndex', { user: 1 });
 })();
