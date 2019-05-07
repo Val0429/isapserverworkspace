28f65418d892c51848661f96c8b847ec8920c9e3
Min.Hsieh <a1991704@yahoo.com.tw>
2019-05-07 10:26:33 +0800

    Add Camera cgi

---
+++ cgi-bin/camera/all.ts
@@ -0,0 +1,49 @@
+import { IUser, Action, Restful, RoleList, Errors, Socket } from 'core/cgi-package';
+import { IRequest, IResponse, IDB } from '../../custom/models';
+import { Print } from '../../custom/helpers';
+import * as Enum from '../../custom/enums';
+
+let action = new Action({
+    loginRequired: true,
+});
+
+export default action;
+
+/**
+ * Action Read
+ */
+type InputR = null;
+
+type OutputR = IResponse.ICamera.IAll[];
+
+action.get(
+    {
+        permission: [RoleList.Admin, RoleList.User],
+    },
+    async (data): Promise<OutputR> => {
+        try {
+            let _input: InputR = data.inputType;
+
+            let query: Parse.Query<IDB.Camera> = new Parse.Query(IDB.Camera);
+
+            let total: number = await query.count();
+
+            let cameras: IDB.Camera[] = await query
+                .limit(total)
+                .find()
+                .fail((e) => {
+                    throw e;
+                });
+
+            return cameras.map((value, index, array) => {
+                return {
+                    objectId: value.id,
+                    name: value.getValue('name'),
+                };
+            });
+        } catch (e) {
+            Print.Log(e, new Error(), 'error');
+            throw e;
+        }
+    },
+);
--- custom/models/response/_index.ts
+++ custom/models/response/_index.ts
@@ -1,10 +1,11 @@
+import * as ICamera from './camera';
 import * as IConfig from './config';
 import * as ILicense from './license';
 import * as ILocation from './location';
 import * as ISetting from './setting';
 import * as IUser from './user';
 
-export { IConfig, ILicense, ILocation, ISetting, IUser };
+export { ICamera, IConfig, ILicense, ILocation, ISetting, IUser };
 
 export interface IPaging {
     total: number;
