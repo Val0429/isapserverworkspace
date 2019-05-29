f6e9ca6db96278a76e56fadbcd4cc89ca30e0987
Min.Hsieh <a1991704@yahoo.com.tw>
2019-05-29 14:25:34 +0800

    Add Random Text helper

--- custom/helpers/utilitys/utility.ts
+++ custom/helpers/utilitys/utility.ts
@@ -145,4 +145,51 @@ export namespace Utility {
             };
         });
     }
+
+    let pools: { num: string[]; EN: string[]; en: string[]; symbol: string[] } = {
+        num: [...Array(10).keys()].map((value, index, array) => {
+            return String.fromCharCode(48 + value);
+        }),
+        EN: [...Array(26).keys()].map((value, index, array) => {
+            return String.fromCharCode(65 + value);
+        }),
+        en: [...Array(26).keys()].map((value, index, array) => {
+            return String.fromCharCode(97 + value);
+        }),
+        symbol: ['!', '@', '#', '$', '%', '&', '*', '+', '-', '?'],
+    };
+    /**
+     * Random Text
+     * @param len
+     * @param option
+     */
+    export function RandomText(len: number, option?: { num?: boolean; EN?: boolean; en?: boolean; symbol?: boolean }): string {
+        try {
+            option = {
+                ...{
+                    num: true,
+                    EN: true,
+                    en: true,
+                    symbol: true,
+                },
+                ...option,
+            };
+
+            let strs: string[] = [];
+            if (option.num) strs.push(...pools.num);
+            if (option.EN) strs.push(...pools.EN);
+            if (option.en) strs.push(...pools.en);
+            if (option.symbol) strs.push(...pools.symbol);
+
+            let str: string = [...Array(len).keys()]
+                .map((value, index, array) => {
+                    return strs[Math.floor(Math.random() * strs.length)];
+                })
+                .join('');
+
+            return str;
+        } catch (e) {
+            throw e;
+        }
+    }
 }
