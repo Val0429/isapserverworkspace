6c0acd1a560d3ae89fc13c4d6afbe95de13b267e
Min.Hsieh <a1991704@yahoo.com.tw>
2019-07-09 15:01:05 +0800

    Add model index

--- custom/shells/create-index.ts
+++ custom/shells/create-index.ts
@@ -8,5 +8,21 @@ Main.ready$.subscribe({
         if (!Config.mongodb.enable) {
             return;
         }
+
+        createIndex('DeviceGroup', 'index', { mode: 1, site: 1, area: 1 });
+        createIndex('Device', 'index', { mode: 1, site: 1, area: 1 });
+        createIndex('LocationArea', 'index', { site: 1 });
+        createIndex('OfficeHour', 'index', { sites: 1 });
+        createIndex('ReportSalesRecord', 'index', { date: 1, site: 1 });
+        createIndex('Tag', 'index', { sites: 1 });
+        createIndex('UserGroup', 'index', { sites: 1 });
+        createIndex('UserInfo', 'index', { user: 1 });
+        createIndex('Weather', 'index', { site: 1 });
+
+        createIndex('ReportDemographicSummary', 'index', { faceId: 1, type: 1, date: 1, site: 1 });
+        createIndex('ReportHumanDetectionSummary', 'index', { type: 1, date: 1, site: 1 });
+        createIndex('ReportHumanDetection', 'index', { date: 1, site: 1, area: 1 });
+        createIndex('ReportPeopleCountingSummary', 'index', { type: 1, date: 1, site: 1 });
+        createIndex('ReportRepeatVisitor', 'index', { type: 1, date: 1, site: 1 });
     },
 });
