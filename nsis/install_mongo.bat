623339248c5d2889f577cb74f60e0a38752a738f
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-07-23 06:08:26 +0800

    Fix batch scripts

--- nsis/install_mongo.bat
+++ nsis/install_mongo.bat
@@ -5,19 +5,21 @@ if not exist "%ProgramW6432%/MongoDB/data_acs" (
 	set init="true"
 )
 echo %init%
+if exist "%ProgramW6432%\MongoDB\acs_mongo.cfg" GOTO installWConfig
 
-if not exist "%ProgramW6432%\MongoDB\acs_mongo.cfg" (
+:installWOConfig
+echo "installWithoutConfig"
 	"%MONGODB_HOME%\mongod" --port 27020 --dbpath "%ProgramW6432%\MongoDB\data_acs" --logpath="%ProgramW6432%\MongoDB\log_acs\log.txt" --install --serviceName "ACS MongoDB" --serviceDisplayName "ACS MongoDB" --replSet rs0		
 	net start "ACS MongoDB"
-	if %init%=="true" goto initRs
-	goto commonExit
-)
+	if "%init%"=="true" GOTO initRs
+GOTO commonExit
 
+:installWConfig
+echo "installWithConfig"
 	"%MONGODB_HOME%\mongod" --config "%ProgramW6432%\MongoDB\acs_mongo.cfg"  --install --serviceName "ACS MongoDB" --serviceDisplayName "ACS MongoDB"		
 	net start "ACS MongoDB"    	
-	if %init%=="true" goto initRs
-		
-goto commonExit
+	if "%init%"=="true" GOTO initRs		
+GOTO commonExit
 
 :initRs
 echo "initRs"
--- nsis/install_mongo_wo_init.bat
+++ nsis/install_mongo_wo_init.bat
@@ -3,10 +3,12 @@ if not exist "%ProgramW6432%/MongoDB/log_acs" mkdir "%ProgramW6432%/MongoDB/log_
 if not exist "%ProgramW6432%/MongoDB/data_acs" mkdir "%ProgramW6432%/MongoDB/data_acs"
 
 if not exist "%ProgramW6432%\MongoDB\acs_mongo.cfg" goto installWOConfig
+echo "installWithConfig"
 "%MONGODB_HOME%\mongod" --config "%ProgramW6432%\MongoDB\acs_mongo.cfg"  --install --serviceName "ACS MongoDB" --serviceDisplayName "ACS MongoDB"
 goto commonExit
 
 :installWOConfig
+echo "installWithOutConfig"
 "%MONGODB_HOME%\mongod" --port 27020 --dbpath "%ProgramW6432%\MongoDB\data_acs" --logpath="%ProgramW6432%\MongoDB\log_acs\log.txt" --install --serviceName "ACS MongoDB" --serviceDisplayName "ACS MongoDB" --replSet rs0
 
 :commonExit
