faafbc2267dc9f5dc7d6109bd86a4671b2d4d580
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-07-25 14:28:03 +0800

    Fix mongo installer

--- nsis/install_mongo.bat
+++ nsis/install_mongo.bat
@@ -9,21 +9,22 @@ if exist "%ProgramW6432%\MongoDB\acs_mongo.cfg" GOTO installWConfig
 
 :installWOConfig
 echo "installWithoutConfig"
-	"%MONGODB_HOME%\mongod" --port 27020 --dbpath "%ProgramW6432%\MongoDB\data_acs" --logpath="%ProgramW6432%\MongoDB\log_acs\log.txt" --install --serviceName "ACS MongoDB" --serviceDisplayName "ACS MongoDB" --replSet rs0		
-	net start "ACS MongoDB"
+	"%MONGODB_HOME%\mongod" --port 27020 --dbpath "%ProgramW6432%\MongoDB\data_acs" --logpath="%ProgramW6432%\MongoDB\log_acs\log.txt" --install --serviceName "ACS MongoDB" --serviceDisplayName "ACS MongoDB" --replSet rs0	
 	if "%init%"=="true" GOTO initRs
 GOTO commonExit
 
 :installWConfig
 echo "installWithConfig"
-	"%MONGODB_HOME%\mongod" --config "%ProgramW6432%\MongoDB\acs_mongo.cfg"  --install --serviceName "ACS MongoDB" --serviceDisplayName "ACS MongoDB"		
-	net start "ACS MongoDB"    	
+	"%MONGODB_HOME%\mongod" --config "%ProgramW6432%\MongoDB\acs_mongo.cfg"  --install --serviceName "ACS MongoDB" --serviceDisplayName "ACS MongoDB"			
 	if "%init%"=="true" GOTO initRs		
 GOTO commonExit
 
 :initRs
 echo "initRs"
+net start "ACS MongoDB"
 "%MONGODB_HOME%\mongo" --port 27020 --eval "rs.initiate({_id : 'rs0', members : [{_id : 0, host : 'localhost:27020'}]})"
 "%MONGODB_HOME%\mongo" --port 27020 --eval "while(true) {if (rs.status().ok) break;sleep(1000)};"
+net stop "ACS MongoDB"
 
 :commonExit
+net start "ACS MongoDB"
