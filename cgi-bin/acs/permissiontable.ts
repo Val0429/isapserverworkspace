92e6307a56e8df860738d1039ceaae2585b49ee7
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-08-05 11:48:08 +0800

    Remove unnecessary includes

--- cgi-bin/acs/permissiontable.ts
+++ cgi-bin/acs/permissiontable.ts
@@ -114,29 +114,12 @@ type OutputR = Restful.OutputR<IPermissionTable>;
 action.get<InputR, OutputR>({ inputType: "InputR" }, async (data) => {
     /// 1) Make Query
     var query = new Parse.Query(PermissionTable)
-        .include("accesslevels.door")
-        .include("accesslevels.doorgroup.doors")
-        .include("accesslevels.timeschedule")
-        .include("accesslevels.reader");
+                .equalTo("system", 0)
+                .include("accesslevels");
 
     let filter = data.parameters as any;
     if(filter.name){
         query.matches("tablename", new RegExp(filter.name), "i");
-    }
-    if(filter.timename){
-        let tsQuery = new Parse.Query(TimeSchedule).matches("timename", new RegExp(filter.timename), "i");    
-        let alQuery = new Parse.Query(AccessLevel).matchesQuery("timeschedule", tsQuery);    
-        query.matchesQuery("accesslevels", alQuery);
-    }
-    if(filter.doorname){
-        let doorQuery = new Parse.Query(Door).matches("doorname", new RegExp(filter.doorname), "i");    
-        let alQuery = new Parse.Query(AccessLevel).matchesQuery("door", doorQuery);
-        query.matchesQuery("accesslevels", alQuery);
-    }
-    if(filter.doorgroupname){
-        let dgQuery = new Parse.Query(DoorGroup).matches("groupname", new RegExp(filter.doorgroupname), "i");    
-        let alQuery = new Parse.Query(AccessLevel).matchesQuery("doorgroup", dgQuery);
-        query.matchesQuery("accesslevels", alQuery);
     }
     /// 2) With Extra Filters
     query = Restful.Filter(query, data.inputType);
