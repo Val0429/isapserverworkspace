4319f49efb25b6c650fcc3e0ee91831ebb009fc0
Val Liu <val.liu@isapsolution.com>
2019-08-06 18:44:08 +0800

    add /mac

---
+++ cgi-bin/mac/index.ts
@@ -0,0 +1,33 @@
+import {
+    express, Request, Response, Router,
+    IRole, IUser, RoleList, IUserKioskData,
+    Action, Errors, O,
+    getEnumKey, omitObject, IInputPaging, IOutputPaging, Restful, UserHelper, ParseObject,
+} from 'core/cgi-package';
+
+import * as os from 'os';
+
+var action = new Action({
+    loginRequired: true,
+    permission: [RoleList.SystemAdministrator, RoleList.Administrator]
+});
+
+/// R: get macs //////////////////////////
+action.get( async (data) => {
+    let interfaces = os.networkInterfaces();
+    const invalidMac: string = "00:00:00:00:00:00";
+    let macs = [];
+    for (let ifkey in interfaces) {
+        let ifinfos = interfaces[ifkey];
+        for (let ifinfo of ifinfos) {
+            ifinfo.mac !== invalidMac && (macs.push(ifinfo.mac));
+        }
+    }
+    macs = macs.filter( (v, i) => macs.indexOf(v) === i );
+    return ParseObject.toOutputJSON(macs);
+});
+///////////////////////////////////////////
+
+export default action;
+
+
