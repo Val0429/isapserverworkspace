06ce19879633fed0e3e11b7facb38a35bbd9c54e
Val Liu <val.liu@isapsolution.com>
2019-08-14 18:00:34 +0800

    update config for Tina

--- config/custom/frs.ts
+++ config/custom/frs.ts
@@ -1,7 +1,7 @@
 var config: Config = {
-    ip: "172.16.10.31",
-    port: 8088,
-    account: "val",
+    ip: "172.16.10.74",
+    port: 80,
+    account: "tina",
     password: "123456"
 }
 export default config;
