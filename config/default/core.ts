c2413dd5f558d69cafb113bd68e2cab16d6254b5
Min.Hsieh <a1991704@yahoo.com.tw>
2019-08-14 18:34:54 +0800

    Upgrade default config

--- config/default/core.ts
+++ config/default/core.ts
@@ -1,7 +1,7 @@
-
 import { Config } from './../../../config_default/core';
 
 var config: Partial<Config> = {
+    publicExternalIP: 'http://localhost:6060',
     port: 6060,
 };
 export default config;
