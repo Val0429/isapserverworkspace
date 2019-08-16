d416284d7727e368280e57da0f5f2e7ab31869d9
Min.Hsieh <a1991704@yahoo.com.tw>
2019-08-16 09:46:09 +0800

    Upgrade server framework

--- custom/models/index.ts
+++ custom/models/index.ts
@@ -1,5 +1,5 @@
 /**
- * ALARM: DON'T MODIFY THIS FILE.
+ * WARNING: DON'T MODIFY THIS FILE.
  * this file is auto created. whatever you insert into this file will be overwritten.
  */
 export * from './base/index';
