2a2e049c049882a8e2994bd7c9deb2aeab889231
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-09-11 05:21:12 +0800

    Add attendance query function

--- custom/services/report-service.ts
+++ custom/services/report-service.ts
@@ -59,6 +59,34 @@ export class ReportService{
         let total = await query.count();
         return {results, total};
     }
+    getAttendanceQuery(filter:any, limit:number=10000, skip:number=0){
+        let query = new Parse.Query(AttendanceRecords)        
+        // .include("member")
+        // .include("door")
+        .exists("member")
+        .exists("door")
+        .equalTo("type", 21)
+        .equalTo("state_id", 2)
+        .exists("card_no")
+        .addAscending("card_no")
+        .addAscending("date_occurred")
+        .addAscending("time_occurred")        
+        .limit(limit)
+        .skip(skip);
+        //console.log("filter attendance", filter);
+        if (filter.CardNumber) {
+            query.matches("card_no", new RegExp(filter.CardNumber), "i");
+        }
+        if (filter.start) {
+            let start = new Date(filter.start);        
+            query.greaterThanOrEqualTo("date_time_occurred", start);
+        }
+        if (filter.end) {
+            let end = new Date(filter.end);
+            query.lessThanOrEqualTo("date_time_occurred", end);
+        }
+        return query;
+    }
     async getAttendanceRecord(filter:any, limit:number=10000, skip:number=0){
         let query = new Parse.Query(AttendanceRecords)
         //.include("member")
