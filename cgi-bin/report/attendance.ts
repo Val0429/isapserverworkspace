9d47c4fe24de0b1265a283def9dc263303a99ffc
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-09-11 05:21:49 +0800

    Change attendance report and data structure

--- cgi-bin/report/attendance.ts
+++ cgi-bin/report/attendance.ts
@@ -1,7 +1,9 @@
-import { Action, Restful} from 'core/cgi-package';
+import { Action, Restful, ParseObject} from 'core/cgi-package';
 
 import { ReportService } from 'workspace/custom/services/report-service';
 import moment = require('moment');
+import MemberService from 'workspace/custom/services/member-service';
+import { mongoDBUrl } from 'helpers/mongodb/url-helper';
 
 
 var action = new Action({
@@ -16,37 +18,59 @@ var action = new Action({
  ********************************/
 
 
-action.get(async (data) => {
-    let pageSize = 10000;
-
-    let filter = data.parameters as any;
-    let reportService = new ReportService();
-    let attendances = await reportService.getAttendanceRecord(filter, pageSize);
-    let cardNumbers = attendances.results.filter(x=>x.card_no && x.card_no != "").map(x=>x.card_no)
-            .filter((value, index, self) => self.indexOf(value)==index)
-            .join(",");
-            
-    filter.CardNumbers=cardNumbers;
+action.post(async (data) => {
     
+    let reportService = new ReportService();
+    let memberService = new MemberService();
+    let filter = data.parameters as any;
+    let pageSize = filter.paging.pageSize || 10;
+    let page = filter.paging.page || 1;
+    let fields = filter.selectedColumns.map(x=>"member."+x.key);
+    fields.push("door.doorname");
+    let attendanceQuery = reportService.getAttendanceQuery(filter, pageSize, (page-1)*pageSize);
+    console.log("filter", filter);
+    attendanceQuery.include("member");
+    attendanceQuery.include("door");
+    attendanceQuery.select(...fields);
+    // const mongoist = require('mongoist');
+    // const db = mongoist(mongoDBUrl());
 
-    let results=[];
-    let i=0;
-    while(i<attendances.results.length){
-        let item = attendances.results[i];
-        let item2 = attendances.results[i+1];
-        i+=2;
+    let memberQuery = memberService.getMemberQuery(filter);
+    attendanceQuery.matchesQuery("member", memberQuery);
+    // console.log("member query",memberQuery.toJSON());
+    // console.log("attendance query", attendanceQuery.toJSON());
+
+    // let records = await db.collection("AttendanceRecord").findAsCursor(attendanceQuery.toJSON().where).skip((page-1)*pageSize).limit(pageSize).toArray();
+    // console.log("records", records);
+
+
+    let total = await attendanceQuery.count();
+    let oAttendances = await attendanceQuery.find();
+    let attendances = oAttendances.map(x=>ParseObject.toOutputJSON(x));
+    
+    
+    for(let item of attendances){        
+        let newDoor = item.door;
+        let newMember = item.member;
+        delete(newMember.objectId);
+        delete(newDoor.objectId);
+        delete(item.member);
+        delete(item.door);
+        //merge fields
+        Object.assign(item, newMember);
+        Object.assign(item, newDoor);
+        //let item2:any = {};        
+        //if(!item2 || item2.card_no != item.card_no)continue;
         
-        if(!item2 || item2.card_no != item.card_no)continue;
-        
-        item.date_time_occurred_end = item2.date_time_occurred;
-        item.at_id_end = item2.at_id;
+        // item.date_time_occurred_end = item2.date_time_occurred;
+        // item.at_id_end = item2.at_id;
         let timeStart = moment(item.date_time_occurred);
         let timeEnd = moment(item.date_time_occurred_end);
         item.startTime = timeStart.format("HH:mm:ss");
         item.dateOccurred = timeStart.format("YYYY-MM-DD");
         item.endTime = timeEnd.format("HH:mm:ss");
         item.workTime = moment.utc(timeEnd.diff(timeStart)).format("H[h ]m[m ]s[s]");
-        results.push(item);
+        
         
     }
 
@@ -55,10 +79,10 @@ action.get(async (data) => {
         paging:{
             page:1,
             pageSize,
-            total:results.length,
-            totalPages:Math.ceil(results.length / pageSize)
+            total,
+            totalPages:Math.ceil(attendances.length / pageSize)
         },
-        results
+        results:attendances
     };
 });
 
