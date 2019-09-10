3895f527d53880430d3b49c1d2be6c1634eb8f5b
Min.Hsieh <a1991704@yahoo.com.tw>
2019-09-10 09:42:13 +0800

    Add HikVision helper

--- custom/helpers/index.ts
+++ custom/helpers/index.ts
@@ -1,3 +1,7 @@
-export * from './frs-service';
+import * as HikVision from './Hikvision';
+
+export * from './frs';
 export * from './push-notifications';
 export * from './utilitys';
+
+export { HikVision };
