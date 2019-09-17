9de3079e300d9323cbdcbe1a99df10e71c46015f
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-09-17 14:21:38 +0800

    Fix query permission

--- cgi-bin/acs/querypermissiontable.ts
+++ cgi-bin/acs/querypermissiontable.ts
@@ -23,18 +23,17 @@ action.get<InputR, OutputR>({ inputType: "InputR" }, async (data) => {
     let page = +paging.page;
     let pageSize = +paging.pageSize;
     /// 1) Make Query
-    var query = new Parse.Query(PermissionTable)
-                .equalTo("system", 800)
-                .ascending("tablename");
+    var query = new Parse.Query(CCureClearance)
+                .ascending("name");
     
     let filter = data.parameters as any;
     if(filter.name){
-        query.matches("tablename", new RegExp(filter.name), "i");
+        query.matches("name", new RegExp(filter.name), "i");
     }
     let total = await query.count();
     let oCcurePermTables = await query.limit(pageSize).skip((page-1)*pageSize).find();
     let results = oCcurePermTables.map(x=>ParseObject.toOutputJSON(x));
-    let ccureQuery = new Parse.Query(PermissionTable).containedIn("tablename", results.map(x=>x.tablename));
+    let ccureQuery = new Parse.Query(PermissionTable).containedIn("tablename", results.map(x=>x.name));
     let memberService = new MemberService();
     let oMembers = await memberService.getMemberQuery({})
                         .include("permissionTable")
@@ -44,15 +43,15 @@ action.get<InputR, OutputR>({ inputType: "InputR" }, async (data) => {
                         .limit(Number.MAX_SAFE_INTEGER)
                         .find();
     let members = oMembers.map(x=>ParseObject.toOutputJSON(x));
-    let onlineCcurePermTables = await new Parse.Query(CCureClearance).containedIn("name", results.map(x=>x.tablename)).limit(results.length).find();
     for(let result of results){
+        result.tablename = result.name;
+        delete(result.name);
         result.doors=[];
         result.members = members.filter(x=>x.permissionTable.find(x=>x.tablename == result.tablename));
         
-        let ccureTable = onlineCcurePermTables.find(x=>x.attributes.name==result.tablename);
-        if(!ccureTable)continue;
+        if(!result.data)continue;
         
-        for(let accessRule of ccureTable.attributes.data){
+        for(let accessRule of result.data){
             if(accessRule.type=="door" ){
                 let isActive = Array.isArray(accessRule.devices) && accessRule.devices.find(x=>x.name.length>2 && x.name.substring(0,2)!="D_");
                 if(!isActive) continue;
